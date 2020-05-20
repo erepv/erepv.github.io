26b890f51333aeb6ae402aefef455082a82d3e24
erepv <65602358+erepv@users.noreply.github.com>
2020-05-20 11:22:54 +0300

    js

---
+++ js/mainCode.js
@@ -0,0 +1,20 @@
+let elemVisibility = {
+    iRotateON: function(iElement, ulElement){
+        iElement.style.transform = 'rotate(180deg)';
+        ulElement.style.display = 'block';},
+    iRotateOFF: function(iElement, ulElement){
+        iElement.style.transform = 'rotate(0deg)';
+        ulElement.style.display = 'none';}
+}
+function toRotate(priceElement){
+    let isOn = priceElement.getAttribute("isOn");
+    let iElem = priceElement.querySelector('h3>i');
+    let ulElem = priceElement.querySelector('h3+ul');
+
+    if (isOn ==='no') {
+        elemVisibility.iRotateON(iElem, ulElem);
+        priceElement.setAttribute('isOn', 'yes')}
+    else {
+        elemVisibility.iRotateOFF(iElem, ulElem);
+        priceElement.setAttribute('isOn', 'no')}   
+}
